309d78a37e3c087ef846212d1c8e2f31bf12ab88
Aziz Sobirov <89915512+Aziz-Sobirov@users.noreply.github.com>
2022-03-23 10:10:08 +0500

    Add files via upload
    23-march

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
+let el = document.getElementById("el")
+let pages = document.querySelectorAll("#el li")
+let scroll = pages[1].offsetLeft
+for(let i=1;i<pages.length-1;i++){
+    pages[i].innerHTML+= `<div class="btns">
+            <button class="as_btn" onclick="prevSlide()">
+               <a href="#">
+                   <i class="far fa-arrow-left"></i> Ortga 
+               </a>
+            </button>
+            <button class="as_btn" onclick="nextSlide()">
+                <a href="#">
+                    Keyingi <i class="far fa-arrow-right"></i>
+                </a>
+            </button>
+        </div>`
+}
+
+el.onscroll = function(){
+    if(Math.floor(el.scrollLeft) >= Math.floor(scroll * 4)){
+        getDate()
+    }
+}
+
+function nextSlide(){
+    el.scroll(el.scrollLeft+scroll,0)
+}
+function prevSlide(){
+    el.scroll(el.scrollLeft-scroll,0)
+}
+
 function getDate(){
   let inp = document.getElementById("date")
   let inp2 = document.getElementById("start-date")
@@ -25,13 +56,3 @@ function getDate(){
     result.innerHTML=`Sizda - ${d} kunlik qazo namozingiz bor`
   }
 }
-        
-        
-        
-        
-        
-//const h = Math.floor(date / 1000 /60 /60) % 24;
-//const m = Math.floor(date / 1000 /60) % 60;
-// const s = Math.floor(date / 1000) %60;
-        
-    
